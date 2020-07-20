8dd6331b3eaa806269809681901220b8a625be20
Alex Towell <queelius@gmail.com>
2020-07-20 11:43:59 -0500

    updates

--- include/interval_arithmetic/disjoint_interval_set2.hpp
+++ include/interval_arithmetic/disjoint_interval_set2.hpp
@@ -72,3 +72,69 @@ namespace alex::math
 			make_disjoint_interval_set<T>(v._set.begin(), v._set.end()));
 	}
 }
+
+
+/*
+	template <typename T>
+	DisjointIntervals<T>::DisjointIntervals(const string& s)
+	{
+		const string N = "[-+]?([0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?|inf(inity)?)";
+		const string L = "(\\(|\\[)";
+		const string R = "(\\)|\\])";
+		const string S = " *";
+		const string INF = "(+|-)?inf(inity)?";
+		const regex r("(" + N + "|" + L + S + N + S + "," + S + N + S + R + ")");
+
+		vector<Interval<T>> ints;
+		for (auto i = sregex_iterator(s.begin(), s.end(), r); i != sregex_iterator(); ++i)
+		{
+			T value[2];
+			bool open[2];
+			T factor;
+			stringstream ss(i->str());
+			auto skip = [&ss]() -> void
+			{
+				while (isspace(ss.peek()))
+					ss.get();
+			};
+			char c;
+
+			if (ss.peek() == '[' || ss.peek() == '(')
+			{
+				skip();
+				open[0] = (ss.get() == '(');
+				for (auto j = 0; j < 2; ++j)
+				{
+					factor = static_cast<T>(1);
+					skip();
+					if (ss.peek() == '+' || ss.peek() == '-')
+						factor = (ss.get() == '-' ? -1 : 1);
+
+					if (ss.peek() == 'i')
+					{
+						value[j] = numeric_limits<T>::infinity();
+						while (isalpha(ss.peek()))
+							ss.get();
+					}
+					else
+						ss >> value[j];
+					value[j] *= factor;
+
+					if (j == 0)
+						ss >> c; // capture comma
+				}
+
+				ss >> c;
+				open[1] = c == ')';
+				ints.push_back((Interval<T>(value[0], value[1], open[0], open[1])));
+			}
+			else
+			{
+				ss >> value[0];
+				ints.push_back(Interval<T>::make_degenerate(value[0]));
+			}
+		}
+
+		*this = DisjointIntervals<T>(ints.begin(), ints.end());
+	}
+/*
