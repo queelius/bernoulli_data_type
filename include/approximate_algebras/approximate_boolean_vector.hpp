6f31b8d9d65b735315c0485ccc3e209c3799fa4b
Alex Towell <queelius@gmail.com>
2020-08-04 07:53:05 -0500

    updates

--- include/approximate_algebras/approximate_boolean_vector.hpp
+++ include/approximate_algebras/approximate_boolean_vector.hpp
@@ -1,24 +1,15 @@
 #pragma once
-#include "../approximate_value/approximate_value.hpp"
-#include "approximate_boolean.hpp"
+#include "../approximate_value/first_order_random_approximate_value.hpp"
 #include <vector>
+#include <array>
 #include <optional>
+#include <random>
 #include <set>
 using std::vector;
 using std::optional;
+using std::array;
 
 /**
- * Approximate Booleans are the simplest case to consider.
- */
-
-using vec_approximate_bool = vector<approximate_bool>;
-
-
-/**
- * The first-order random approximate set is equivalent to
- * a vec_approximate_bool in which every Boolean element
- * has the same error rate.
- * 
  * The second-order random approximate set in which the
  * positive elements have an error rate fnr and the
  * negative elements have an error rate fpr is equivalent to
@@ -143,3 +134,124 @@ int order(vec_approximate_bool const & a)
         alphas.insert(x.alpha);
     return alphas.size();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Consider the Boolean algebra
+ *     (array<bool,N>, +, *, ~, array<bool,N>{false}, array<bool,N>{true}).
+ * 
+ * Note that
+ *     (array<bool,N>, +, *, ~, array<bool,N>{false}, array<bool,N>{true})
+ * is the same as
+ *     (array<random_approximate<0,bool>,N>, +, *, ~, array<bool,N>{false}, array<bool,N>{true}).
+ * 
+ * However, we may approximate this Boolean algebra with a first-order random
+ * approximate Boolean algebra
+ *     (array<random_approximate<1,bool>,N>, +, *, ~, array<bool,N>{false}, array<bool,N>{true}).
+ * 
+ * Define
+ *     contains : array<T,N> -> int -> bool
+ * by
+ *     contains(A,i) := A[i] == true.
+ * 
+ * Now, we may ask questions, such as what is the probability that
+ *     contains(array<random_approximate<1,bool>,N>{A}, i)
+ * is not equal to
+ *     contains(array<bool,N>{A}, i)
+ * for any i in the {0,...,N-1}? Assuming each first-order random approximate
+ * Boolean has the same error rate E, then the answer is simply E.
+ * 
+ * What do we wish to call this random structure? It is a vector of N first
+ * order random approximate Booleans, which models the concept of
+ *     array<random_approximate<1,bool>,N>
+ * with an error rate E on the contains function.
+ * 
+ * Note that it does not model the concept of
+ *     random_approximate<1,array<bool,N>>
+ * since this would apply to the entire value type of the vector, i.e.,
+ * the error rate would be on the equality predicate ==. However, the
+ * equality predicate also has an error rate for 
+ *     array<random_approximate<1,bool>,N>,
+ * but that is a lot more complicated.
+ * 
+ * We choose to provide a template specialization for this particular
+ * type.
+ */
+template <typename T, size_t N>
+struct first_order_random_approximate_vector {};
+
+template <size_t N>
+struct first_order_random_approximate_vector<bool,N>
+{
+    array<bool,N> given;
+    double contains_error;
+
+    auto contains(int x) const
+    {
+        return random_approximate<1,bool>{value[x],contains_error};
+    }
+
+    template <typename R>
+    auto sample(R & r) const
+    {
+        std::bernoulli_distribution ber(contains_error);
+        array<bool,N> s;
+        for (size_t i = 0; i < N; ++i)
+        {
+            if (ber(r))
+                s[i] = !given[i];
+            else
+                s[i] = given[i];
+        }
+        return first_order_approximate_vector<bool,N>{s,contains_error};
+    }
+};
+
+template <size_t N>
+struct first_order_approximate_vector<bool,N>
+{
+    array<bool,N> value;
+    double contains_error;
+
+    auto contains(int x) const
+    {
+        return approximate<1,bool>{value[x],contains_error};
+    }
+
+    auto operator==(first_order_approximate_vector<bool,N> const & rhs)
+    {
+        for (size_t i = 0; i < N; ++i)
+        {
+            // auto p = approximate<1,bool>{rhs.value[i]} == approximate<1,bool>{value[i]};
+            // ???
+        }
+    }
+};
+
+
+
+
+
---
+++ include/approximate_value/first_order_random_approximate_value.hpp
@@ -0,0 +1,103 @@
+#include "random_approximate_value.hpp"
+
+template <>
+struct random_approximate<1,bool>
+{
+    using value_type = bool;
+    constexpr int order = 1;
+
+    double epsilon;
+    bool given;
+
+    auto fpr() const { return epsilon; }
+    auto fnr() const { return epsilon; }
+    auto error() const { return epsilon; }
+
+    auto operator()() const { return given; }
+
+    template <typename R>
+    auto sample(R & r)
+    {
+        std::bernoulli_distribution ber(epsilon);
+        if (given)
+            return approximate<1,bool>{epsilon, 1-ber(r)};
+        else
+            return approximate<1,bool>{epsilon, ber(r)};
+    }
+};
+
+/**
+ * A realization of a value sampled from the random_approximate<1,bool>
+ * random approximate value model.
+ */
+struct approximate<1,bool>
+{
+    using value_type = bool;
+    auto constexpr order = 1;
+
+    bool value;
+    double err;
+
+    auto operator==(bool x) const { return approximate<1,bool>{value==x,err}; }
+    auto operator!=(bool x) const { return approximate<1,bool>{value!=x,err}; }
+
+    double fpr() const { return err; }
+    double fnr() const { return err; }
+
+    /**
+     * Suppose
+     *     X ~ random_approximate<1,bool> and
+     *     Y ~ random_approximate<0,bool>.
+     * Then, by the first-order random approximate Boolean model,
+     *     P[X != a|Y == a] == error(X). 
+     * 
+     * We are given an observation of X rather than Y, but we are
+     * interested in
+     *     alpha := P[Y == a|X == a]
+     * which, by the law of probability
+     *     alpha = P[Y == a,X == a] / P[X == a]
+     *           = P[X == a|Y == a] P[Y == a] / P[X == a]
+     *           = (1-error(X)) P[Y == a] / P[X == a].
+     * 
+     * By the total law of probability,
+     *     P[X == a] = P[X == a,Y == a] + P[X == a,Y != a]
+     *               = P[X == a|Y == a]P[Y == a] + P[X == a|Y != a]P[Y != a]
+     *               = (1-error(X))P[Y == a] + error(X)(1-P[Y == a]).
+     * 
+     * If we denote P[Y == a] by p(a) and substitute P[X == a] with the above,
+     * we get the result
+     *     alpha(p) =        p(1-error(X))
+     *                ---------------------------.
+     *                p(1-error(X))+(1-p)error(X)
+     */
+    auto error_rate(double p = .5)
+    {
+        // note: error_rate(.5) = err
+        return (1-p)*err / (p*(1-err) + (1-p)*err);
+    }
+
+    bool operator() const { return value; }
+    auto operator()() const { return value; }
+};
+
+auto operator~(approximate<1,bool> a)
+{
+    return approximate<1,bool>{!a.value,a.err};
+}
+
+// at some point, for higher orders, just need to start
+// using intervals to span the fpr and fnr.
+auto operator&(approximate<1,bool> a, approximate<1,bool> b)
+{
+    if (a.value && b.value)
+        return approximate<3,bool>{a.alpha+b.alpha-a.alpha*b.alpha, true};
+
+    if (!a.value && !b.value)
+        return approximate<3,bool>{a.alpha*b.alpha, false};
+
+    if (!a.value && b.value)
+        return approximate<3,bool>{a.alpha*(1.-b.alpha), false};
+
+    // a.value && !b.value
+    return approximate<3,bool>{(1.-a.alpha)*b.alpha, false};
+}
